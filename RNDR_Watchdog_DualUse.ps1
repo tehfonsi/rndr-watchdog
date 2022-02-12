ce3fc321f5238905c25d8ee602e6074f1200a4fc
Stephan Schober <teh.fonsi@gmail.com>
2022-02-12 20:53:31 +0100

    fix: Copy RNDR_Watchdog_info.ps1 after update

--- RNDR_Watchdog_DualUse.ps1
+++ RNDR_Watchdog_DualUse.ps1
@@ -1,7 +1,7 @@
 # RNDR Watchdog (Dual Use Version)
 # Filename: RNDR_Watchdog_DualUse.ps1
 
-$Release = "0.4.0"
+$Release = "0.4.1"
 
 # This Windows Powershell script ensures the RenderToken RNDRclient.exe (RNDR) is running at all time and allows to start/shutdown an alternative workload (Dual) when the client signals it is idle.
 # The RNDR client won't process any job if the GPUs are under load or VRAM is used, therefore the Dual workload needs to be shut down completely before rendering.
@@ -444,6 +444,9 @@ Function Fix-Defect-Userconfig {
 
         # Overwrite the script RNDR_Watchdog_DualUse.ps1 with the latest version
         Copy-Item $currentPath\$WatchdogGithubRepoName\RNDR_Watchdog_DualUse.ps1 $currentPath -Force
+
+        # Overwrite the script RNDR_Watchdog_Info.ps1 with the latest version
+        Copy-Item $currentPath\$WatchdogGithubRepoName\RNDR_Watchdog_Info.ps1 $currentPath -Force
 
         # Try to backup the defect userconfig file
         Copy-Item $currentPath\RNDR_Watchdog_Userconfig.ini $currentPath\RNDR_Watchdog_Userconfig.bak -Force -ErrorAction SilentlyContinue
